956f8b835daff3930165f7c94d20f65aa5721286
WeijiaMa123 <31965430+WeijiaMa123@users.noreply.github.com>
2017-09-19 14:23:43 -0400

    changes

---
+++ chapter 2 alarm problem.py
@@ -0,0 +1,7 @@
+#Write a Python program to solve the general version of the above problem.
+#Ask the user for the time now (in hours 0-24), and ask for the number of hours to wait.
+#Your program should output what the time will be on the clock when the alarm goes off.
+tw= int (input ("numbers of hours to wait"))
+tn= int (input ("what time is now"))
+a = (tw + tn) % 24
+print (a , ":00")
---
+++ chapter2 (6).py
@@ -0,0 +1,7 @@
+print (5 % 2)
+print(9 % 5)
+print(12 % 15)
+print(6 % 6)
+print(0 % 7)
+print(7 % 0) # we can not divide or moduel by 0
+
